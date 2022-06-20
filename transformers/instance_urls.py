5f7669c773184de9f1e8164b391c39bfdd4c8358
dave <thegr8dave@gmail.com>
2022-06-20 23:29:51 +0100

    sources

--- transformers/instance_urls.py
+++ transformers/instance_urls.py
@@ -2,4 +2,7 @@ peertube_instances = ["https://video.manicphase.me",
                       "https://diode.zone",
                       "https://tilvids.com",
                       "https://videos.trom.tf",
-                      "https://watch.breadtube.tv"]
+                      "https://watch.breadtube.tv",
+                      "https://tube.jeena.net",
+                      "https://peertube.linuxrocks.online",
+                      "https://conf.tube"]
